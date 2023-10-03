68da8308d20a2eb239eeed35385aa1a0591a0f54
Ethan Mikel <ethanmikel@Ethans-MacBook-Pro.local>
2023-10-03 08:36:23 -0500

    branching new

---
+++ TX-Luminescence/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace base_folders_files.Controllers
+{
+    // Controllers/HomeController.cs
+
+    public class HomeController : Controller
+    {
+        public ActionResult CurrentProjects()
+        {
+            // Logic to fetch and prepare data for the "CurrentProjects" page
+            var projects = GetProjects(); // Example: Load current projects
+            return View("CurrentProjects", projects);
+        }
+
+        public ActionResult Events()
+        {
+            // Logic to fetch and prepare data for the "Events" page
+            var events = GetEvents(); // Example: Load all events
+            return View("Events", events);
+        }
+
+        public ActionResult FAQs()
+        {
+            // Logic to fetch and prepare data for the "FAQs" page
+            var faqs = GetFAQs(); // Example: Load all FAQs
+            return View("FAQs", faqs);
+        }
+    }
+}
---
+++ TX-Luminescence/Models/SourceModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace TX_Luminescence.Models
+{
+    public abstract class SourceModel
+    {
+    }
+}
